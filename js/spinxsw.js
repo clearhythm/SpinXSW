c18e1f740926b862edb3709e7bcb810cc047529e
Erik Burns <erikburns@server>
2014-02-07 18:32:54 -0800

    playing with node

--- js/spinxsw.js
+++ js/spinxsw.js
@@ -140,7 +140,7 @@ var setLightsListener = function(){
   ws.onmessage = function (event) { // respond to node.js notifications coming back
    // console.log('onmessage', event);
    var degrees = event.data.data;
-   console.log('event data', event.data);
+   console.log('event data', event);
    active_light = Math.floor(degrees / light_increment);
    // for now, only update the lights if user moves into a new light quadrant
    if (active_light != current_light) {
